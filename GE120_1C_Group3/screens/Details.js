12f22a4ae4940a055c863ce4d92fa565e0f7e006
Dominic Jr Fargas <dcfargas@up.edu.ph>
2024-06-12 17:05:27 +0800

    rename points and point to imagePath

--- GE120_1C_Group3/screens/Details.js
+++ GE120_1C_Group3/screens/Details.js
@@ -2,15 +2,30 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet, Image } from 'react-native';
 
+// List of Images
+import point1 from '../data/photos/point1.jpg';
+import point2 from '../data/photos/point2.jpg';
+import point3 from '../data/photos/point3.jpg';
+// Add more imports as needed
+
+
+
 const Details = ({ navigation, route }) => {
 
   // Destructure item from route params
   const { item } = route.params;
+  const images = [
+    point1,
+    point2,
+    point3,
+    // Add more images to the array as needed
+  ];
+  const imagePath = images[item.pointNumber - 1]; // Adjust indexing based on your pointNumber starting index
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Place Details</Text>
-      <Image source={{ uri: item.image }} style={styles.image} />
+      <Image source={imagePath} style={styles.image} />
       <Text style={styles.label}>Name: <Text style={styles.value}>{item.name}</Text></Text>
       <Text style={styles.label}>Address: <Text style={styles.value}>{item.address}</Text></Text>
       <Text style={styles.label}>Latitude: <Text style={styles.value}>{item.latitude}</Text></Text>
