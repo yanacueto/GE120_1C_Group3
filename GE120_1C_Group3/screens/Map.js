b313a13776b206a2da79398b079be06794b21482
Dominic Jr Fargas <dcfargas@up.edu.ph>
2024-06-12 15:54:23 +0800

    add a map

--- GE120_1C_Group3/screens/Map.js
+++ GE120_1C_Group3/screens/Map.js
@@ -2,14 +2,37 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
+// Import necessary components from react-native-maps
+import MapView, { Marker } from 'react-native-maps';
+
 const Map = ({ navigation }) => {
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Use React Native Maps here</Text>
-      <Button
-        title="Go to Point Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+    <View style={{ flex: 1 }}>
+      {/* Use MapView to render the map */}
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 14.655361,
+          longitude: 121.061804,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {/* Marker for the current location */}
+        <Marker
+          coordinate={{ latitude: 14.655361, longitude: 121.061804 }}
+          title="Current Location"
+        />
+        {/* Marker for the specific point */}
+        <Marker
+          coordinate={{ latitude: 14.655361, longitude: 121.071804 }}
+          title="Specific Point"
+          onPress={() => {
+            navigation.navigate('Details')
+          }}
+        />
+      </MapView>
     </View>
   );
 };
